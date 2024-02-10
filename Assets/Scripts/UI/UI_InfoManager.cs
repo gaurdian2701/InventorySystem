26982019f1febc612e562521935242fb6817dc39
kiran2701 <gaurdian.kiran@gmail.com>
2024-02-10 12:13:28 +0000

    Added Buying/Selling Panel which is shown when the player chooses to buy/sell something

--- Assets/Scripts/UI/UI_InfoManager.cs
+++ Assets/Scripts/UI/UI_InfoManager.cs
@@ -7,9 +7,16 @@ using UnityEngine.UI;
 
 public class UI_InfoManager : MonoBehaviour
 {
+    [Header("UI Panels")]
     [SerializeField] private GameObject itemInfoPanel;
+    [SerializeField] private GameObject buySellPanel;
+
+    [Header("Text Info")]
     [SerializeField] private TextMeshProUGUI currencyText;
     [SerializeField] private TextMeshProUGUI weightText;
+    [SerializeField] private TextMeshProUGUI buySellInfoHeading;
+    [SerializeField] private TextMeshProUGUI buySellInfoText;
+    [SerializeField] private TextMeshProUGUI buySellButtonText;
 
     private GameObject inventoryPanel;
     private GameObject shopPanel;
@@ -17,10 +24,23 @@ public class UI_InfoManager : MonoBehaviour
     private InventoryService inventoryService;
     private ShopService shopService;
 
+    private ItemScriptableObject currentItemSelected;
+    private int itemAmountSelected;
+
     private TextMeshProUGUI[] itemInfoPanelTexts;
+
+    private enum CurrentAction
+    {
+        BUY,
+        SELL
+    };
+
+    private CurrentAction currentAction;
+
     private void Awake()
     {
         itemInfoPanel.SetActive(false);
+        buySellPanel.SetActive(false);
 
         EventService.Instance.onItemUIClickedEvent.AddEventListener(PopUpInfoPanel);
         EventService.Instance.onInventoryUpdated.AddEventListener(UpdateCurrencyAndWeight);
@@ -32,11 +52,12 @@ public class UI_InfoManager : MonoBehaviour
         shopService = StorageService.Instance.shopService;
 
         itemInfoPanelTexts = itemInfoPanel.GetComponentsInChildren<TextMeshProUGUI>();
+        itemAmountSelected = 0;
     }
 
     private void UpdateCurrencyAndWeight(int currency, float weight)
     {
-        currencyText.text = currency.ToString();    
+        currencyText.text = currency.ToString();
         weightText.text = weight.ToString();
     }
 
@@ -45,20 +66,32 @@ public class UI_InfoManager : MonoBehaviour
         ItemScriptableObject item;
         if (layer == inventoryPanel.layer)
         {
+            currentAction = CurrentAction.SELL;
             item = inventoryService.inventoryItems[itemIndex];
             ShowInfo(item, item.sellingPrice);
         }
         else if (layer == shopPanel.layer)
         {
+            currentAction = CurrentAction.BUY;
             item = shopService.shopItems[itemIndex];
             ShowInfo(item, item.buyingPrice);
         }
         else
+        {
+            if (buySellPanel.activeInHierarchy)
+                buySellPanel.SetActive(false);
+
             itemInfoPanel.SetActive(false);
+            currentItemSelected = null;
+            itemAmountSelected = 0;
+        }
     }
 
     private void ShowInfo(ItemScriptableObject itemInfo, int price)
     {
+        buySellButtonText.text = currentAction.ToString();
+        currentItemSelected = itemInfo;
+
         itemInfoPanel.SetActive(true);
         itemInfoPanelTexts[0].text = itemInfo.name.ToString();
         itemInfoPanelTexts[1].text = itemInfo.type.ToString();
@@ -66,5 +99,28 @@ public class UI_InfoManager : MonoBehaviour
         itemInfoPanelTexts[3].text = itemInfo.rarity.ToString();
         itemInfoPanelTexts[4].text = "Weight: " + itemInfo.weight.ToString();
         itemInfoPanelTexts[5].text = "Price: " + price.ToString() + " Qty: " + itemInfo.quantity.ToString();
+    }
+
+    public void ShowBuySellPanel()
+    {
+        buySellPanel.SetActive(true);
+        buySellInfoHeading.text = $"HOW MANY DO YOU WANT TO {currentAction}?";
+        UpdateByIncrease();
+    }
+
+    public void UpdateByIncrease()
+    {
+        if (itemAmountSelected >= currentItemSelected.quantity)
+            return;
+
+        buySellInfoText.text = $"{++itemAmountSelected} {currentItemSelected.name} FOR {itemAmountSelected * currentItemSelected.sellingPrice} coins";
+    }
+  
+    public void UpdateByDecrease()
+    {
+        if (itemAmountSelected <= 1)
+            return;
+
+        buySellInfoText.text = $"{--itemAmountSelected} {currentItemSelected.name} FOR {itemAmountSelected * currentItemSelected.sellingPrice} coins";
     }
 }
